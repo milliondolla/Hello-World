b7c3b0e25aaef50054a28242a6e377595a574944
Theron, Gizelle : Barclays Africa Group Limited <Gizelle.Theron@absa.co.za>
2018-10-05 12:45:43 +0200

    change package names

---
+++ CrudWeb/src/za/co/milliondolla/data/Employee.java
@@ -0,0 +1,22 @@
+package za.co.milliondolla.data;
+
+public class Employee {
+	private String employeeId;
+	private String employeeName;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+}
