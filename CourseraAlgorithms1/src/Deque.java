ad853a3eaf4267c4377d04dff89797f961afa769
Theron, Gizelle : Barclays Africa Group Limited <Gizelle.Theron@absa.co.za>
2018-12-06 10:56:27 +0200

    Impl Deque api

--- CourseraAlgorithms1/src/Deque.java
+++ CourseraAlgorithms1/src/Deque.java
@@ -1,47 +1,122 @@
 import java.util.Iterator;
 
-public class Deque<T> implements Iterable<T> {
+public class Deque<Item> implements Iterable<Item> {
+
+	// Maintain pointer to first and last nodes in a linked list
+	private Node first, last = null;
+	private int sz = 0;
+
+	// inner class
+	private class Node {
+		Item item;
+		Node next;
+		Node prev;
+	}
 
 	// construct an empty deque
 	public Deque() {
-		
+
 	}
 
+	// is the deque empty?
+	public boolean isEmpty() {
+		return (first == null || last == null);
+	}
+
+	// return the number of items on the deque
+	public int size() {
+		return sz;
+	}
+
+	// add the item to the front
+	public void addFirst(Item item) {
+
+		if (item == null)
+			throw new java.lang.IllegalArgumentException();
+
+		Node oldFirst = first;
+
+		Node newFirst = new Node();
+		newFirst.item = item;
+		newFirst.next = oldFirst;
+
+		oldFirst.prev = newFirst;
+
+		first = newFirst;
+		sz++;
+
+	}
+
+	// add the item to the end
+	public void addLast(Item item) {
+
+		if (item == null)
+			throw new java.lang.IllegalArgumentException();
+
+		// the same as enqueue
+		Node oldlast = last;
+		last = new Node();
+		last.item = item;
+		last.next = null;
+		last.prev = oldlast;
+		if (isEmpty())
+			first = last;
+		else
+			oldlast.next = last;
+
+		sz++;
+
+	}
+
+	// remove and return the item from the front
+	public Item removeFirst() {
+		// same as dequeue (pop)
+		if (isEmpty()) {
+			throw new java.util.NoSuchElementException();
+		} else {
+			Item item = first.item;
+			first = first.next;
+
+			if (isEmpty()) {
+				first = last;
+			} else {
+				first.prev = null;
+			}
+
+			sz--;
+			return item;
+		}
+	}
+
+	// remove and return the item from the end
+	public Item removeLast() {
+
+		if (isEmpty()) {
+			throw new java.util.NoSuchElementException();
+		} else {
+
+			Node oldLast = last;
+
+			last = oldLast.prev;
+
+			if (isEmpty()) {
+				first = last;
+			} else {
+				last.next = null;
+			}
+
+			return oldLast.item;
+		}
+
+	}
+
+	// return an iterator over items in order from front to end
 	@Override
-	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
+	public Iterator<Item> iterator() {
 		return null;
 	}
 
-//	// is the deque empty?
-//	public boolean isEmpty() {
-//	}
-//
-//	// return the number of items on the deque
-//	public int size() {
-//	}
-//
-//	// add the item to the front
-//	public void addFirst(Item item) {
-//	}
-//
-//	// add the item to the end
-//	public void addLast(Item item) {
-//	}
-//
-//	// remove and return the item from the front
-//	public Item removeFirst() {
-//	}
-//
-//	// remove and return the item from the end
-//	public Item removeLast() {
-//	}
-//
-//	// return an iterator over items in order from front to end
-//	public Iterator<Item> iterator() {
-//	}
-//
-//	// unit testing (optional)
-//	public static void main(String[] args) {
-//	}
+	// unit testing (optional)
+	public static void main(String[] args) {
+	}
 }
